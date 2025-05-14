b4c81f56e88941854e9c26738ba472fe68ae80f3
Ben Zanin <gnomon-@users.noreply.github.com>
2025-05-14 13:06:08 -0400

    Add province/territory specific holidays
    Notably missing Orangemen's Day in Newfoundland and Labrador, because
    I have not yet figured out how to express its scheduling properly with
    the holiday.el date calculation facilities.
    
    For holidays celebrated in more than a single province or territory, I
    rigged up a truly terrible little chunk of code to decode a table of
    two letter province codes into add-to-list invocations.
    
    I also specifically changed the description of Good Friday to make a
    bit more obvious the difference between the religious Christian
    holiday captured by holiday-easter-etc in holidays.el and the secular
    federal holiday observed in Canada (other than Quebec); absent this
    name change it looks weird to see Good Friday appear twice in the
    calendar or the org-mode agenda view.

--- canadian-holidays.el
+++ canadian-holidays.el
@@ -30,9 +30,103 @@ file to display holidays with the performance bump it promises, it seems
 that some `holiday-float' entries like Victoria Day will not appear, for
 reasons I do not currently understand.)
 
+cf. also the province/territory specific holidays in each of:
+
+  `holiday-canadian--ab'
+  `holiday-canadian--bc'
+  `holiday-canadian--mb'
+  `holiday-canadian--nb'
+  `holiday-canadian--nl'
+  `holiday-canadian--ns'
+  `holiday-canadian--nt'
+  `holiday-canadian--nu'
+  `holiday-canadian--on'
+  `holiday-canadian--pe'
+  `holiday-canadian--qc'
+  `holiday-canadian--sk'
+  `holiday-canadian--yt'
 ")
 
 ;; https://www.canada.ca/en/revenue-agency/services/tax/public-holidays.html
 ;; https://canada-holidays.ca
 ;; https://www.timeanddate.com/holidays/canada/
 ;; https://www.statutoryholidays.com
+
+(defvar holiday-canadian--bc
+  '()
+  "Canadian statutory holidays observed in British Columbia.")
+
+(defvar holiday-canadian--ab
+  '()
+  "Canadian statutory holidays observed in Alberta.")
+
+(defvar holiday-canadian--sk
+  '()
+  "Canadian statutory holidays observed in Saskatchewan.")
+
+(defvar holiday-canadian--mb
+  '((holiday-float  2  1  3 "Louis Riel Day"))
+  "Canadian statutory holidays observed in Manitoba.")
+
+(defvar holiday-canadian--on
+  '((holiday-fixed 12 26 "Boxing Day"))
+  "Canadian statutory holidays observed in Ontario.")
+
+(defvar holiday-canadian--qc
+  '((holiday-easter-etc 1 "Easter Monday")
+    (holiday-fixed  6 24 "Fête Nationale (St. Jean Baptiste Day)"))
+  "Canadian statutory holidays observed in Quebec.")
+
+(defvar holiday-canadian--nl
+  '((holiday-fixed  3 17 "St. Patrick's Day")
+    (holiday-fixed  4 23 "St. George's Day")
+    (holiday-fixed  6 24 "Discovery Day"))
+  ;; FIXME: Orangemen's Day: what is the precise date specification?
+  ;; The best I can find now is "the Monday closest to July 12".
+  "Canadian statutory holidays observed in Newfoundland and Labrador.")
+
+(defvar holiday-canadian--nb
+  '()
+  "Canadian statutory holidays observed in New Brunswick.")
+
+(defvar holiday-canadian--ns
+  '((holiday-float  2  1  3 "Heritage Day"))
+  "Canadian statutory holidays observed in Nova Scotia.")
+
+(defvar holiday-canadian--pe
+  '((holiday-float  2  1  3 "Islander Day"))
+  "Canadian statutory holidays observed in Prince Edward Island.")
+
+(defvar holiday-canadian--yt
+  '()
+  "Canadian statutory holidays observed in Yukon.")
+
+(defvar holiday-canadian--nt
+  '((holiday-fixed  6 21 "National Aboriginal Day")
+    (holiday-fixed  7  9 "Nunavut Day"))
+  "Canadian statutory holidays observed in Northwest Territories.")
+
+(defvar holiday-canadian--nu
+  '()
+  "Canadian statutory holidays observed in Nunavut.")
+
+(seq-do
+ (lambda (provhols)
+   (let ((provinces (seq-filter
+		     (lambda (candidate)
+		       (not (string-equal "__" candidate)))
+		     (car provhols)))
+	 (holiday (cadr provhols)))
+     (seq-do
+      (lambda (province)
+	(add-to-list (intern (concat "holiday-canadian--" (symbol-name province))) holiday))
+      provinces)))
+ '(((bc ab sk mb on __ nl nb ns pe nt nu) (holiday-easter-etc  -2 "Good Friday (Canadian statutory holiday)"))
+   ((bc ab sk __ on __ __ nb __ __ __ __) (holiday-float  2  1  2 "Family Day"))
+   ((bc ab sk mb on qc __ __ __ __ nt nu) (holiday-float  5  1 -1 "Victoria Day" 25))
+   ((__ __ __ __ __ qc __ __ __ __ __ __) (holiday-float  5  1 -1 "National Patriotes Day" 25))
+   ((bc ab sk mb on qc __ nb ns pe nt nu) (holiday-fixed  7  1    "Canada Day"))
+   ((__ __ __ __ __ __ nl __ __ __ __ __) (holiday-fixed  7  1    "Memorial Day"))
+   ((bc ab sk __ on __ __ nb __ __ __ nu) (holiday-float  8  1  1 "Civic Holiday"))
+   ((bc ab sk mb on qc __ __ __ __ nt nu) (holiday-float 10  1  2 "Canadian Thanksgiving"))
+   ((bc ab sk mb __ __ __ nb __ pe nt nu) (holiday-fixed 11 11    "Remembrance Day"))))
